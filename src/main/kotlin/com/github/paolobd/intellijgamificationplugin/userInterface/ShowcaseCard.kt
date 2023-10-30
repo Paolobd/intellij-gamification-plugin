65cc09aecb4d113caf57d996e350397a97236908
Paolo Stefanut Bodnarescul <paolobodnarescul@gmail.com>
2023-10-30 17:34:34 +0100

    Fixed showcasecard

--- src/main/kotlin/com/github/paolobd/intellijgamificationplugin/userInterface/ShowcaseCard.kt
+++ src/main/kotlin/com/github/paolobd/intellijgamificationplugin/userInterface/ShowcaseCard.kt
@@ -3,10 +3,7 @@ package com.github.paolobd.intellijgamificationplugin.userInterface
 import com.github.paolobd.intellijgamificationplugin.enums.GlobalAchievement
 import com.github.paolobd.intellijgamificationplugin.services.ApplicationStatePersistence
 import java.awt.Font
-import javax.swing.BorderFactory
-import javax.swing.BoxLayout
-import javax.swing.JLabel
-import javax.swing.JPanel
+import javax.swing.*
 
 class ShowcaseCard(var id: Int) {
     val card = JPanel()
@@ -16,45 +13,58 @@ class ShowcaseCard(var id: Int) {
     init {
         card.layout = BoxLayout(card, BoxLayout.Y_AXIS)
 
-        val innerCard = JPanel()
-        innerCard.layout = BoxLayout(innerCard, BoxLayout.Y_AXIS)
-        innerCard.border = BorderFactory.createEtchedBorder()
-
         setContent(id)
 
+        val innerCard = JPanel()
+        innerCard.border = BorderFactory.createEtchedBorder()
         innerCard.add(iconLabel)
+
+        val milestoneCard = JPanel()
+        milestoneCard.layout = BoxLayout(milestoneCard, BoxLayout.X_AXIS)
+        milestoneCard.add(Box.createHorizontalGlue())
+        milestoneCard.add(milestoneLabel)
+        milestoneCard.add(Box.createHorizontalGlue())
+
         card.add(innerCard)
-        card.add(milestoneLabel)
+        card.add(milestoneCard)
     }
 
     private fun setContent(id: Int) {
-        milestoneLabel.font = Font(milestoneLabel.font.name, Font.PLAIN, 10)
+        iconLabel.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+
         milestoneLabel.border = BorderFactory.createEmptyBorder(5, 0, 0, 0)
+
         if (id > 0) {
-            val achEnum = GlobalAchievement.values()[id]
-            iconLabel.toolTipText = achEnum.achievement.name
-            iconLabel.icon = IconUtility().loadGlobalAchIcon(achEnum.achievement.iconPath)
+            val achievement = GlobalAchievement.values().map { it.achievement }.first { it.id == id }
+            iconLabel.toolTipText = achievement.name
+            iconLabel.icon = IconUtility().loadGlobalAchIcon(achievement.iconPath)
+
+            milestoneLabel.font = Font(milestoneLabel.font.name, Font.PLAIN, 10)
 
             updateProgressShowcaseCard()
         } else {
             iconLabel.toolTipText = null
-            milestoneLabel.text = null
+
+            milestoneLabel.font = Font(milestoneLabel.font.name, Font.ITALIC, 10)
+            milestoneLabel.text = "Empty"
             iconLabel.icon = IconUtility().loadEmptyIcon()
         }
     }
 
     fun updateProgressShowcaseCard() {
-        val achEnum = GlobalAchievement.values()[id]
+        if (id <= 0) return
+
+        val achievement = GlobalAchievement.values().map { it.achievement }.first { it.id == id }
         val exp = ApplicationStatePersistence.getInstance().state.achievementList.first { it.id == id }.currentExp
         var i = 0
-        while (i < achEnum.achievement.milestone.size - 1 && exp >= achEnum.achievement.milestone[i]) {
+        while (i < achievement.milestone.size - 1 && exp >= achievement.milestone[i]) {
             i++
         }
 
-        if (i == achEnum.achievement.milestone.size - 1 && exp >= achEnum.achievement.milestone[i]) {
+        if (i == achievement.milestone.size - 1 && exp >= achievement.milestone[i]) {
             milestoneLabel.text = "Completed"
         } else {
-            milestoneLabel.text = "$exp / ${achEnum.achievement.milestone[i]}"
+            milestoneLabel.text = "$exp / ${achievement.milestone[i]}"
         }
     }
 
